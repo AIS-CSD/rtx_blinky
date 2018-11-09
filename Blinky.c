44fc08708eb54ab72f222d580ef81ac41c3ce8b9
Phisanupong Meepragob <prototype2.dld@gmail.com>
2018-11-09 13:51:46 +0700

    version 2

--- Blinky.c
+++ Blinky.c
@@ -25,6 +25,7 @@ osThreadId tid_lcd;                     /* Thread id of thread: lcd          */
 #define LED_D   3
 #define LED_CLK 7
 
+#define LED_M 	4
 #define LED_G   3
 /*----------------------------------------------------------------------------
  *      Switch LED on
